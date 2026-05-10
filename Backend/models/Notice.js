50a94c5fdb1497610a02c087db24988aab318108
yashp6483 <yashp4710@gmail.com>
2026-05-10 22:39:57 +0530

    git ci/cd

--- Backend/models/Notice.js
+++ Backend/models/Notice.js
@@ -5,7 +5,7 @@ const noticeSchema = new mongoose.Schema({
         type : String,
         required : true
     },
-    description : {
+    description : { 
         type : String,
         required : true
     },
